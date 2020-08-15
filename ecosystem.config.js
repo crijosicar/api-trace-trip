fa12e38d0add9b19ec59d329f5887044514508b4
crijosicar <12100677+crijosicar@users.noreply.github.com>
2020-08-14 21:43:33 -0500

    created ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API Services | TrazeTrip',
+      script: 'npm',
+      args: 'start',
+      cwd: '/var/www/api-trace-trip/',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '200M',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      interpreter: '/home/csierra/.nvm/versions/node/v12.18.1/bin/node',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3001,
+      },
+      env_stating: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+    },
+  ],
+  deploy: {
+    stating: {
+      user: 'crijosicar',
+      host: '64.227.61.21',
+      ref: 'origin/stage',
+      repo: 'git@github.com/api-trace-trip.git',
+      path: '/var/www/api-trace-trip',
+      'post-deploy':
+        'npm install && rimraf dist && nest build && pm2 reload ecosystem.config.js --env stating',
+    },
+  },
+};
