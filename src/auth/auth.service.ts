cfb60c3dc6f137fdf6d8de78b86f923b062492a1
Cristian Sierra <crijosicar@Crijosicars-MacBook-Pro.local>
2020-09-02 19:00:45 -0500

    testing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,8 +15,9 @@ export class AuthService {
   async validateUser(email: string, password: string): Promise<Partial<User>> {
     const user = await this.usersService.findOne(email);
 
-    if (user && (await compare(password, user.password)))
+    if (user && (await compare(password, user.password))){
       return omit(user, 'password');
+    }
 
     return null;
   }
